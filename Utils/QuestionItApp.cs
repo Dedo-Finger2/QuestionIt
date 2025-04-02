497e0c348bf3252a3b473197a7ab8defafe1738c
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-02 06:56:29 -0300

    feat: adds QuestionItApp class properties and constructor

---
+++ Utils/QuestionItApp.cs
@@ -0,0 +1,23 @@
+﻿namespace QuestionIt.Utils
+{
+    public class QuestionItApp
+    {
+        private int questionnaireListPerPage;
+        private int questionnaireListCurrentPage;
+        private int currentQuestionId;
+        private int currentQuestionnaireId;
+        private int[] userAnswers;
+        // TODO: Implement
+        // private QuestionnaireService questionnaireService;
+        // private ResultService resultService;
+
+        public QuestionItApp()
+        {
+            this.questionnaireListCurrentPage = 0;
+            this.questionnaireListCurrentPage = 1;
+            this.currentQuestionId = 0;
+            this.currentQuestionnaireId = 0;
+            this.userAnswers = [];
+        }
+    }
+}
