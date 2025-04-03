cbc217dc9e87672a96441f160336bc8b651b3b4e
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-03 09:03:15 -0300

    feat: adds IQuestionRepository interface

---
+++ Repositories/IQuestionRepository.cs
@@ -0,0 +1,9 @@
+﻿using QuestionIt.Entities;
+
+namespace QuestionIt.Repositories
+{
+    public interface IQuestionRepository : ICrudSaveOperation<QuestionEntity>, ICrudFindOneByIdOperation<QuestionEntity, int>
+    {
+        public QuestionEntity[] FindAllByQuestionnaireId(int questionnaireId);
+    }
+}
