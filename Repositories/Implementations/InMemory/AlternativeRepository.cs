78c0775fc4ea5b6c07e5fe2b2d2e7297c2188884
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-03 20:20:09 -0300

    feat: adds InMemorryAlternativeRepository

---
+++ Repositories/Implementations/InMemory/AlternativeRepository.cs
@@ -0,0 +1,31 @@
+﻿using QuestionIt.Entities;
+
+namespace QuestionIt.Repositories.Implementations.InMemory
+{
+    public class AlternativeRepository : IAlternativeRepository
+    {
+        private AlternativeEntity[] alternatives;
+
+        public AlternativeRepository()
+        {
+            this.alternatives = [];
+        }
+
+        public AlternativeEntity[] FindAllByQuestionId(int questionId)
+        {
+            return this.alternatives.Where(a => a.QuestionId == questionId).ToArray();
+        }
+
+        public AlternativeEntity? FindOneById(int id)
+        {
+            return this.alternatives.Where(a => a.Id == id).First();
+        }
+
+        public AlternativeEntity Save(AlternativeEntity entity)
+        {
+            this.alternatives = this.alternatives.Append(entity).ToArray();
+
+            return entity;
+        }
+    }
+}
