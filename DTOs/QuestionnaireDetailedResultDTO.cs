7fec6e37e888378f0105170eb0b4aa4f1996e685
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-03 08:11:12 -0300

    feat: adds QuestionnaireDetailedResultDTO

---
+++ DTOs/QuestionnaireDetailedResultDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace QuestionIt.DTOs
+{
+    public class QuestionnaireDetailedResultDTO
+    {
+        public string QuestionnaireTitle { get; private set; }
+        public QuestionnaireResultAnswerDTO[] QuestionsAndAnswers { get; private set; }
+        public int UserPoints { get; private set; }
+        public int MaxPoints { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public QuestionnaireDetailedResultDTO() { }
+        
+        public QuestionnaireDetailedResultDTO(string questionnaireTitle, QuestionnaireResultAnswerDTO[] questionsAndAnswers, int userPoints, int maxPoints, DateTime createdAt)
+        {
+            this.QuestionnaireTitle = questionnaireTitle;
+            this.QuestionsAndAnswers = questionsAndAnswers;
+            this.UserPoints = userPoints;
+            this.MaxPoints = maxPoints;
+            this.CreatedAt = createdAt;
+        }
+    }
+}
