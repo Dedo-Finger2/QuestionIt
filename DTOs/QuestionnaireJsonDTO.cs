23187e124a03ee87f14e9ca55c8cc35c9fb96c07
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-03 07:59:27 -0300

    feat: adds QuestionnaireJsonDTO

---
+++ DTOs/QuestionnaireJsonDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace QuestionIt.DTOs
+{
+    public class QuestionnaireJsonDTO
+    {
+        public string Title { get; private set; }
+        public string? Description { get; private set; }
+        public QuestionnaireQuestionJsonDTO[] QuestionnaireQuestions { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+
+        public QuestionnaireJsonDTO() { }
+
+        public QuestionnaireJsonDTO(string title, string? description, QuestionnaireQuestionJsonDTO[] questionnaireQuestions, DateTime createdAt, DateTime? updatedAT)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.QuestionnaireQuestions = questionnaireQuestions;
+            this.CreatedAt = createdAt;
+            this.UpdatedAt = updatedAT;
+        }
+    }
+}
