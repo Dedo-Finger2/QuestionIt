a7465a177c1017cb0f701fefaad9e71ec2f1e707
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-04 09:44:16 -0300

    feat: adds InMemoryResult repository

---
+++ Repositories/Implementations/InMemory/ResultRepository.cs
@@ -0,0 +1,32 @@
+﻿using QuestionIt.DTOs;
+using QuestionIt.Entities;
+
+namespace QuestionIt.Repositories.Implementations.InMemory
+{
+    public class ResultRepository : IResultRepository
+    {
+        private ResultEntity[] results;
+
+        public ResultRepository()
+        {
+            this.results = [];
+        }
+
+        public ResultEntity[] FindAllByQuestionnaireId(int questionnaireId)
+        {
+            return this.results.Where(r => r.QuestionnaireId == questionnaireId).ToArray();
+        }
+
+        public ResultEntity? FindOneById(int id)
+        {
+            return this.results.Where(r => r.Id == id).ToArray().Length == 0 ? null : this.results.Where(r => r.Id == id).First();
+        }
+
+        public ResultEntity Save(ResultEntity entity)
+        {
+            this.results = this.results.Append(entity).ToArray();
+
+            return entity;
+        }
+    }
+}
